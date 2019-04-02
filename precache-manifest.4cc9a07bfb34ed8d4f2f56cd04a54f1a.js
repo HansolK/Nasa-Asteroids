42349253f7e5734d71cf1264da07f3f68fa808be
Hansol Kim <hansol-kim@live.com>
2019-04-02 11:39:49 +1100

    Updates

---
+++ precache-manifest.4cc9a07bfb34ed8d4f2f56cd04a54f1a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e5f97b70e6d76d80cdd2a215c85d59f",
+    "url": "/Nasa-Asteroids/static/media/user-solo-stroke.0e5f97b7.svg"
+  },
+  {
+    "revision": "0ec96c3339dc44d5d595ac1a64b93847",
+    "url": "/Nasa-Asteroids/static/media/home-fill.0ec96c33.svg"
+  },
+  {
+    "revision": "e15cd87512482d8da73c",
+    "url": "/Nasa-Asteroids/static/js/runtime~main.048e66d4.js"
+  },
+  {
+    "revision": "0af04f6997bbe35393c8",
+    "url": "/Nasa-Asteroids/static/js/main.9bb9e56e.chunk.js"
+  },
+  {
+    "revision": "1bd4b73acb6d4ed4b8b2",
+    "url": "/Nasa-Asteroids/static/js/2.6bbbaef0.chunk.js"
+  },
+  {
+    "revision": "0af04f6997bbe35393c8",
+    "url": "/Nasa-Asteroids/static/css/main.2bfa5270.chunk.css"
+  },
+  {
+    "revision": "1bd4b73acb6d4ed4b8b2",
+    "url": "/Nasa-Asteroids/static/css/2.52c75941.chunk.css"
+  },
+  {
+    "revision": "ab4b81de2d4bde3106c1c0ba2cadd496",
+    "url": "/Nasa-Asteroids/index.html"
+  }
+];
